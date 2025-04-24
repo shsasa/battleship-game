58678d59723e7f48d3e66bdc8b7e4b4d09e17bb3
Sayed husain <shsasa@gmail.com>
2025-04-25 02:34:07 +0300

    add addCellToBox

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 let gameStart = false
+
+const botBox = document.getElementById('bot')
+const playerBox = document.getElementById('player')
+
+const addCellToBox = (box) => {
+  for (let i = 1; i <= 100; i++) {
+    const cell = document.createElement('div')
+    cell.classList = 'cell'
+    cell.id = box.id + i
+
+    box.appendChild(cell)
+  }
+}
+
+addCellToBox(playerBox)
+addCellToBox(botBox)
